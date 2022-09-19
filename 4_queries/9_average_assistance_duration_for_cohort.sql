bbf6ae6516096b9915ae525fa8a3056551fd09d9
Nicholas <nc_chau@hotmail.com>
2022-09-19 23:17:27 +0000

    assistance request queries

---
+++ 4_queries/9_average_assistance_duration_for_cohort.sql
@@ -0,0 +1,9 @@
+SELECT avg(total_duration) as average_total_duration
+FROM (
+  SELECT sum(completed_at - started_at) as total_duration
+  FROM assistance_requests
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+) as total_durations;
